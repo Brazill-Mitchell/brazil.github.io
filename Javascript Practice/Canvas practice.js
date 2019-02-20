ca48c8d4e877298d90431dfe87ec7f609711eeae
brazill91 <44531123+brazill91@users.noreply.github.com>
2019-02-19 23:52:07 -0500

    Add Color & Brush Size option to Canvas Practice
    Added Color and Brush size selection options to canvas

--- Javascript Practice/Canvas practice.js
+++ Javascript Practice/Canvas practice.js
@@ -39,7 +39,17 @@ function testPaint(){
         let paintCanvas = document.getElementById("canvas1");
         let ctxt = paintCanvas.getContext("2d");
         ctxt.fillStyle = "blue";
-        ctxt.fillRect(getRelativePos()[0],getRelativePos()[1],2,2);
+        ctxt.fillRect(getRelativePos()[0],getRelativePos()[1],brushSize,brushSize);
+    }
+}
+
+function drawCircle(){
+    if(mouseState === 1){
+        let paintCanvas = document.getElementById("canvas1");
+        let ctx = paintCanvas.getContext("2d");
+        ctx.fillStyle = brushColor;
+        ctx.beginPath(); ctx.arc(getRelativePos()[0],getRelativePos()[1],brushSize,0,2*Math.PI);
+        ctx.fill();
     }
 }
     
@@ -53,6 +63,21 @@ function mouseDown(){
 function mouseUp(){
     mouseState = 0;
 };
+
+var brushSize = 5;
+var brushColor = "rgba(0,0,0,1)";
+
+function setBrushSize(){
+    let size = document.getElementById("brush-size-menu");
+    brushSize = size.value;
+    document.getElementById("brush-size-display").innerHTML = brushSize; 
+}
+
+function setBrushColor(){
+    let color = document.getElementById("brush-color-menu");
+    brushColor = color.value;
+    document.getElementById("brush-color-display").innerHTML = brushColor;
+}
 
 
 
